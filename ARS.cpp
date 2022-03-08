9b63098849fc5eea78586c467f48efb6cb2a3216
Sreehitha varma karukuri <sreehitha.karukuri@gmail.com>
2022-03-08 16:23:58 +0530

    Spark Airways

---
+++ ARS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include "sparkairways.cpp"
+using namespace std;
+int main()
+{
+	cout<<"\n   -------------------------------------------------------------------------------------------"<<endl;
+	cout << " \t SPARK AIRWAYS -- AIRLINE RESERVATION SYSTEM";
+	cout<<"\n   -------------------------------------------------------------------------------------------"<<endl;
+	menu();
+	seat_fill();
+	return 0;
+}
